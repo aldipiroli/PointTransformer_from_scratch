3be62f34777908451cdd52bcda97ca75f6845f11
Aldi Piroli <aldipiroli@gmail.com>
2025-11-29 13:00:19 +0100

    added batched knn

---
+++ point_transformer/test/test_ops.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import numpy as np
+import torch
+
+from point_transformer.utils.operations import find_kNN, find_kNN_sklearn
+
+
+def test_find_kNN():
+    B = 2
+    N = 64
+    K = 8
+    x = torch.randn(B, N, 3)
+    indicies = find_kNN(x, k=K)
+    assert indicies.shape == (B, N, K)
+
+
+def test_kNN_methods():
+    B = 2
+    N = 64
+    K = 8
+    x = torch.randn(B, N, 3)
+    indicies_torch = find_kNN(x, k=K)
+
+    indicies_sklearn = []
+    for i in range(B):
+        idx = find_kNN_sklearn(x[i], K)
+        indicies_sklearn.append(idx)
+    indicies_sklearn = torch.tensor(np.array(indicies_sklearn))
+    assert torch.equal(indicies_sklearn, indicies_torch)
+
+
+if __name__ == "__main__":
+    test_kNN_methods()
+    print("All tests passed!")
--- point_transformer/utils/operations.py
+++ point_transformer/utils/operations.py
@@ -1,7 +1,14 @@
+import torch
 from sklearn.neighbors import NearestNeighbors
 
 
-def find_kNN(p, K):
-    nbrs = NearestNeighbors(n_neighbors=K, algorithm="ball_tree").fit(p)
-    distances, indices = nbrs.kneighbors(p)
+def find_kNN_sklearn(x, k):
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm="ball_tree").fit(x)
+    distances, indices = nbrs.kneighbors(x)
     return indices  # NxK
+
+
+def find_kNN(x, k):
+    dist = torch.cdist(x, x)
+    distances, indicies = torch.topk(dist, k=k, dim=-1, largest=False, sorted=True)
+    return indicies
